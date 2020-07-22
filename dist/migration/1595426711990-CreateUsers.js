8d478dfe8bfa5512ede4faba722e35a81d77540b
朱骏杰 <ories.zhu@mochain.vip>
2020-07-22 22:36:27 +0800

    create users 成功

--- dist/migration/1595426711990-CreateUsers.js
+++ dist/migration/1595426711990-CreateUsers.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.CreatePost1595163052652 = void 0;
+exports.CreateUsers1595426711990 = void 0;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
@@ -17,12 +17,12 @@ var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/creat
 
 var _typeorm = require("typeorm");
 
-var CreatePost1595163052652 = /*#__PURE__*/function () {
-  function CreatePost1595163052652() {
-    (0, _classCallCheck2["default"])(this, CreatePost1595163052652);
+var CreateUsers1595426711990 = /*#__PURE__*/function () {
+  function CreateUsers1595426711990() {
+    (0, _classCallCheck2["default"])(this, CreateUsers1595426711990);
   }
 
-  (0, _createClass2["default"])(CreatePost1595163052652, [{
+  (0, _createClass2["default"])(CreateUsers1595426711990, [{
     key: "up",
     value: function () {
       var _up = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(queryRunner) {
@@ -30,28 +30,24 @@ var CreatePost1595163052652 = /*#__PURE__*/function () {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
-                _context.next = 2;
-                return queryRunner.createTable(new _typeorm.Table({
-                  name: 'posts',
+                queryRunner.createTable(new _typeorm.Table({
+                  name: 'users',
                   columns: [{
                     name: 'id',
-                    type: 'int',
-                    isPrimary: true,
                     isGenerated: true,
-                    generationStrategy: 'increment'
+                    type: 'int',
+                    generationStrategy: 'increment',
+                    isPrimary: true
                   }, {
-                    name: 'title',
+                    name: 'username',
                     type: 'varchar'
                   }, {
-                    name: 'content',
-                    type: 'text'
+                    name: 'password_digest',
+                    type: 'varchar'
                   }]
                 }));
 
-              case 2:
-                return _context.abrupt("return", _context.sent);
-
-              case 3:
+              case 1:
               case "end":
                 return _context.stop();
             }
@@ -73,13 +69,9 @@ var CreatePost1595163052652 = /*#__PURE__*/function () {
           while (1) {
             switch (_context2.prev = _context2.next) {
               case 0:
-                _context2.next = 2;
-                return queryRunner.dropTable('posts');
+                queryRunner.dropTable('users');
 
-              case 2:
-                return _context2.abrupt("return", _context2.sent);
-
-              case 3:
+              case 1:
               case "end":
                 return _context2.stop();
             }
@@ -94,7 +86,7 @@ var CreatePost1595163052652 = /*#__PURE__*/function () {
       return down;
     }()
   }]);
-  return CreatePost1595163052652;
+  return CreateUsers1595426711990;
 }();
 
-exports.CreatePost1595163052652 = CreatePost1595163052652;
+exports.CreateUsers1595426711990 = CreateUsers1595426711990;
--- src/migration/1595426711990-CreateUsers.ts
+++ src/migration/1595426711990-CreateUsers.ts
@@ -2,19 +2,19 @@ import {MigrationInterface, QueryRunner, Table} from 'typeorm'
 
 export class CreateUsers1595426711990 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.createTable(new Table({
-            name: 'users',
-            columns: [
-                {name: 'id', isGenerated: true, type: 'int', generationStrategy: 'increment', isPrimary: true},
-                {name: 'username', type: 'varchar'},
-                {name: 'password_digest', type: 'varchar'},
-            ]
-        }))
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {name: 'id', isGenerated: true, type: 'int', generationStrategy: 'increment', isPrimary: true},
+        {name: 'username', type: 'varchar'},
+        {name: 'password_digest', type: 'varchar'},
+      ]
+    }))
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.dropTable('users');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('users')
+  }
 
 }

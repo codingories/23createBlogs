8e9fccc8763c375163d0d9b8b3f200e571f3a8a5
朱骏杰 <ories.zhu@mochain.vip>
2020-07-27 21:17:02 +0800

    利用 axios 成功发送数据，但是目前没有接口所以报错

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -1,5 +1,6 @@
 import {NextPage} from 'next'
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import axios from 'axios'
 
 const SignUp: NextPage = () => { // 利用NextPage初始化注册页面
   const [formData, setFormData] = useState({
@@ -7,12 +8,16 @@ const SignUp: NextPage = () => { // 利用NextPage初始化注册页面
     password: '',
     passwordConfirmation: ''
   })
-
+  const onSubmit = useCallback((e)=>{
+    e.preventDefault()
+    axios.post(`/ap1/v1/users`, formData)
+  },[formData]) // []不加参数参数，表示只在页面第一次创建渲染创建onSubmit函数,其它时候ui怎么变,onSubmit不变
+  // [formData] 表示formData变onSubmit也变,不加打印出来就是空，加了才有值
   return (
     <>
       <h1>注册</h1>
       {JSON.stringify(formData)}
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label>
             用户名
@@ -27,22 +32,22 @@ const SignUp: NextPage = () => { // 利用NextPage初始化注册页面
         <div>
           <label>
             密码
-            <input type="text" value={formData.password}
-               onChange={e=> setFormData({
-                 ...formData,
-                 password: e.target.value
-               })}
+            <input type="password" value={formData.password}
+                   onChange={e=> setFormData({
+                     ...formData,
+                     password: e.target.value
+                   })}
             />
           </label>
         </div>
         <div>
           <label>
             重置密码
-            <input type="text" value={formData.passwordConfirmation}
-               onChange={e=> setFormData({
-                 ...formData,
-                 passwordConfirmation: e.target.value
-               })}
+            <input type="password" value={formData.passwordConfirmation}
+                   onChange={e=> setFormData({
+                     ...formData,
+                     passwordConfirmation: e.target.value
+                   })}
             />
           </label>
         </div>

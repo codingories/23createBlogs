540c10e91a31557640abab056d86f8504fa13394
朱骏杰 <ories.zhu@mochain.vip>
2020-07-28 21:03:15 +0800

    将 users.tsx 中验证的逻辑封装到 User.ts 中

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -6,46 +6,25 @@ import md5 from 'md5'
 const Posts: NextApiHandler = async (req, res) => {
   const {username, password, passwordConfirmation} = req.body
   const connection = await getDatabaseConnection() // 连接数据库
-
-  const errors = {
-    username: [] as string[], password: [] as string[], passwordConfirmation: [] as string[]
-  }
-
-  if(username.trim()===''){
-    errors.username.push('不能为空')
-  }
-  if(!/[a-zA-Z0-9]/.test(username.trim())){
-    errors.username.push('格式不合法')
-  }
-  if(username.trim()>42){
-    errors.username.push('太长')
-  }
-  if(username.trim()<=3){
-    errors.username.push('太短')
-  }
-  const found = connection.manager.findOne(User, {username})
-
-  if(found){
-    errors.username.push('已经存在,不能重复注册')
-  }
-  if(password ===''){
-    errors.passwordConfirmation.push('不能为空')
-  }
-  if(password !== passwordConfirmation){
-    errors.passwordConfirmation.push('两次密码不一致')
-  }
-  const hasErrors = Object.values(errors).find(v=>v.length>0)
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
-  if(hasErrors){
+
+  const user = new User()
+
+  console.log('---------------')
+  console.log(passwordConfirmation)
+  console.log(password)
+
+  user.username = username.trim()
+  user.password = md5(password) // 和明文存差不多,碰撞，彩虹表，真正做产品不能md5
+  user.password = password // 和明文存差不多,碰撞，彩虹表，真正做产品不能md5
+  user.passwordConfirmation = passwordConfirmation // 和明文存差不多,碰撞，彩虹表，真正做产品不能md5
+  user.validate()
+
+  if(await user.hasErrors){
     res.statusCode = 422
-    res.write(JSON.stringify(errors))
+    res.write(JSON.stringify(user.errors))
   }else{
-    const user = new User()
-    user.username = username.trim()
-    user.passwordDigest = md5(password) // 和明文存差不多,碰撞，彩虹表，真正做产品不能md5
-
     await connection.manager.save(user) // 保存用户
-
     res.statusCode = 200
     res.write(JSON.stringify(user))
   }
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,7 @@
 import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Unique, UpdateDateColumn} from 'typeorm'
 import {Post} from './Post'
 import {Comment} from './Comment'
+import {getDatabaseConnection} from '../../lib/getDatabaseConnection'
 
 @Entity('users')
 @Unique(['username'])
@@ -19,4 +20,42 @@ export class User {
   posts: Post[];
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[]
+  errors = {
+    username: [] as string[], password: [] as string[],
+    passwordConfirmation: [] as string[]
+  }
+  password: string;
+  passwordConfirmation: string; // 这两个字段不是数据库，但是是这个类上
+
+  async validate(){
+    if(this.username.trim()===''){
+      this.errors.username.push('不能为空')
+    }
+    if(!/[a-zA-Z0-9]/.test(this.username.trim())){
+      this.errors.username.push('格式不合法')
+    }
+    if(this.username.trim().length>42){
+      this.errors.username.push('太长')
+    }
+    if(this.username.trim().length<=3){
+      this.errors.username.push('太短')
+    }
+    const found = (await getDatabaseConnection()).manager
+      .find(User, {username:this.username})
+
+    if(found){
+      this.errors.username.push('已经存在,不能重复注册')
+    }
+    if(this.password ===''){
+      this.errors.passwordConfirmation.push('不能为空')
+    }
+    if(this.password !== this.passwordConfirmation){
+      this.errors.passwordConfirmation.push('两次密码不一致')
+    }
+  }
+
+  hasErrors(){
+    return !!Object.values(this.errors).find(v => v.length > 0)
+  }
+
 }

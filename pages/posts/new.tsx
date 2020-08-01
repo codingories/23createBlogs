50f7eb790bab8bb897da992fd90ab3184ca46fed
朱骏杰 <ories.zhu@mochain.vip>
2020-08-01 20:18:18 +0800

    创建 posts new 页面 并封装Form组件

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,9 +1,55 @@
 import {NextPage} from 'next'
+import {Form} from '../../components/form'
+import {useCallback, useState} from 'react'
+import axios, {AxiosResponse} from 'axios'
 
 const PostsNew: NextPage = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    content:[]
+  })
+
+  const [errors, setErrors] = useState({
+    title:[], content:[]
+  })
+
+  const onChange = useCallback((key, value)=>{
+    setFormData({
+      ...formData,
+      [key]: value // [key]如果不加[]，就是"key"
+    })
+  },[formData])
+
+  const onSubmit = useCallback((e)=>{
+    e.preventDefault()
+    axios.post(`/api/v1/posts`, formData)
+      .then(()=>{
+        alert('提交成功!')
+      },(error)=>{
+        if(error.response){
+          const response: AxiosResponse = error.response;
+          // if(response)
+          if(response.status === 422){
+            setErrors(response.data);
+          }
+        }
+      })
+  },[formData]) // []不加参数参数，表示只在页面第一次创建渲染创建onSubmit函数,其它时候ui怎么变,onSubmit不变
+
+
   return (
     <div>
-      PostsNew
+      <Form fields={[
+        {label:'标题',type:'text', value: formData.title,
+          onChange: e=>onChange('username', e.target.value)
+          , errors: errors.title },
+        {label:'内容',type:'textarea', value: formData.title,
+          onChange: e=>onChange('username', e.target.value)
+          , errors: errors.title },
+      ]
+      } onSubmit={onSubmit} buttons={<>
+        <button type="submit">提交</button>
+      </>} />
     </div>
   )
 }

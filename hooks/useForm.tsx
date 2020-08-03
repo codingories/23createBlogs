02ccae31a68ee72eff27afcc2a3f683fcd27da2e
朱骏杰 <ories.zhu@mochain.vip>
2020-08-03 21:23:26 +0800

    onSubmit写完

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -1,6 +1,6 @@
 import {useCallback, useState} from 'react'
 
-export function useForm<T>(initFormData:T){ // 通过参数反推T,useForm里面有个T,T的类型就是initFormData的类型
+export function useForm<T>(initFormData: T, onSubmit: (fd: T) => void) { // 通过参数反推T,useForm里面有个T,T的类型就是initFormData的类型
 
   // 非受控
   const [formData, setFormData] = useState(initFormData)
@@ -10,22 +10,27 @@ export function useForm<T>(initFormData:T){ // 通过参数反推T,useForm里面
     // const e: { [k: string]: string[] } = {};
     // const e: { [k: keyof T]: string[] } = {};
     // [key in keyof T] = k in ('title'|'password')
-    const e: { [key in keyof T]?: string[] } = {}; // 死记硬背, key的下标全部都是T的下标
+    const e: { [key in keyof T]?: string[] } = {} // 死记硬背, key的下标全部都是T的下标
     for (let key in initFormData) {
-      if (initFormData.hasOwnProperty(key)){
+      if (initFormData.hasOwnProperty(key)) {
         e[key] = []
       }
     }
     return e // 只做初始值
   })
 
-  const onChange = useCallback((key: keyof T, value: any)=>{
+  const onChange = useCallback((key: keyof T, value: any) => {
     // 看到keyof 就脑补成, 'username'|'password'
     setFormData({
       ...formData,
       [key]: value // [key]如果不加[]，就是"key"
     })
-  },[formData])
+  }, [formData])
+
+  const _onSubmit = useCallback((e) => {
+    e.preventDefault() // 调用外面submit
+    onSubmit(formData)
+  }, [onSubmit, formData])
 
   const form = (
     <form>1</form>

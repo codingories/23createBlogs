2a1151a8cdec86eabc144ba8c74ba6cdee895fd7
朱骏杰 <ories.zhu@mochain.vip>
2020-08-06 21:28:44 +0800

    从首页拷贝代码到posts页面，实现展示posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,34 +1,36 @@
-import {NextPage} from 'next';
-import {getPosts} from '../../lib/posts';
-import Link from 'next/link';
-import {Post} from '../../src/entity/Post'
+import {GetServerSideProps, NextPage} from 'next';
+import {getDatabaseConnection} from 'lib/getDatabaseConnection'
+import {Post} from 'src/entity/Post'
+import Link from 'next/link'
+
 
 type Props = {
-  posts: Post[];
+  posts: Post[],
 }
 const PostsIndex: NextPage<Props> = (props) => {
   const {posts} = props;
   return (
     <div>
       <h1>文章列表</h1>
-      {posts.map(p => <div key={p.id}>
-        <Link href={`/posts/${p.id}`}>
+      {posts.map(post =>
+        <Link key={post.id} href={`/posts/${post.id}`}>
           <a>
-            {p.id}
+            {post.title}
           </a>
         </Link>
-      </div>)}
+      )}
     </div>
   );
 };
-
 export default PostsIndex;
 
-export const getStaticProps = async () => {
-  const posts = await getPosts();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const connection = await getDatabaseConnection()
+  const posts = await connection.manager.find(Post)
+  // console.log('connect1')
   return {
     props: {
-      posts: JSON.parse(JSON.stringify(posts))
+      posts : JSON.parse(JSON.stringify(posts))
     }
   };
 };

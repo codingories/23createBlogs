258fad246aaf20a79f149e77767bb780262cdb09
朱骏杰 <ories.zhu@mochain.vip>
2020-08-06 21:31:27 +0800

    成功实现首页复用代码

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,5 @@
-import {GetServerSideProps, NextPage} from 'next';
-import {getDatabaseConnection} from '../lib/getDatabaseConnection'
-import {Post} from '../src/entity/Post'
-import Link from 'next/link'
+import PostsIndex, {getServerSideProps} from './posts'
 
+export default PostsIndex
 
-type Props = {
-  posts: Post[],
-}
-const index: NextPage<Props> = (props) => {
-  const {posts} = props;
-  return (
-    <div>
-      <h1>文章列表</h1>
-      {posts.map(post =>
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <a>
-            {post.title}
-          </a>
-        </Link>
-      )}
-    </div>
-  );
-};
-export default index;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connection = await getDatabaseConnection()
-  const posts = await connection.manager.find(Post)
-  // console.log('connect1')
-  return {
-    props: {
-      posts : JSON.parse(JSON.stringify(posts))
-    }
-  };
-};
+export {getServerSideProps}

cb49491dd1bd033f2f160de826389d79b8f320b8
朱骏杰 <ories.zhu@mochain.vip>
2020-08-15 20:29:47 +0800

    删除成功

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,30 +1,46 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import {GetServerSideProps, GetServerSidePropsContext, NextPage} from 'next'
 import {getDatabaseConnection} from '../../lib/getDatabaseConnection'
 import {Post} from '../../src/entity/Post'
 import marked from 'marked'
 import Link from 'next/link'
 import {withSession} from '../../lib/withSession'
-
+import axios from 'axios'
 type Props = {
+  id: number;
   post: Post,
   currentUser: User | null
 }
 const postsShow: NextPage<Props> = (props) => {
-  const {post, currentUser} = props
+  const {post, currentUser, id} = props
+  const onRemove = useCallback(()=>{
+     axios.delete(`/api/v1/posts/${id}`).then(()=>{
+        window.alert('删除成功')
+     },()=>{
+       window.alert('删除失败')
+     })
+  },[id])
   return (
     <>
       <div className="wrapper">
         <h1>{post.title}</h1>
         { currentUser &&
-        <p>
+        <p className="actions">
           <Link href="/posts/[id]/edit" as={`/posts/${post.id}/edit`}><a>编辑</a></Link>
+          {/*<Link href="/posts/[id]/edit" as={`/posts/${post.id}/edit`}><a>编辑</a></Link>*/}
+          <button onClick={onRemove} >删除</button>
         </p>
         }
         <article className="markdown-body" dangerouslySetInnerHTML={{__html: marked(post.content)}}>
         </article>
       </div>
-      <style jsx>{`
+      <style jsx>{` 
+        .actions > *{
+          margin: 4px;
+        }
+        .actions > *:first-child{
+          margin-left: 0;
+        }
         .wrapper {
           max-width: 800px;
           margin: 16px auto;
@@ -41,11 +57,13 @@ export default postsShow
 
 export const getServerSideProps: GetServerSideProps<any, { id: string }> = withSession(async (context: GetServerSidePropsContext) => {
   const connection = await getDatabaseConnection()
-  const post = await connection.manager.findOne('Post', context.params.id)
+  const id = context.params.id
+  const post = await connection.manager.findOne('Post', id)
   const currentUser = (context.req as any).session.get('currentUser') || null;
 
   return {
     props: {
+      id: parseInt(id.toString()),
       post: JSON.parse(JSON.stringify(post)),
       currentUser
     }

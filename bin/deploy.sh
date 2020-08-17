0197b94cc695b0eb67883f8c35684adb0bdb55e0
朱骏杰 <ories.zhu@mochain.vip>
2020-08-17 20:24:29 +0800

    update deploy1.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,8 @@
-echo Hello > /tmp/frank
+docker start 534 &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false
+yarn build &&
+docker build -t fang/node-web-app . &&
+docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
+echo 'OK!'
